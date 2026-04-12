2aefc87d51e9b402ef38fb3ab21bed8878dea9f9
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:28:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/drhelius/Gearboy/refs/heads/master/platforms/macos/image.png
 export DEPLOY_GTK=1
